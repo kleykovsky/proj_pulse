5264a56292b2588bdc36f16d4e1fe810363c365e
Alex <kleykovsky@gmail.com>
2022-03-28 17:35:16 +0500

    finish adaptive  section carousel, catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,67 @@
+	//Слайдер на js
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    nav: false,
-    navPosition: "bottom",
-    controls: false,
-    autoplay: false,
-    // responsive: {
-    //     640: {
+(function($) {
+	$(function(){
 
-    //     },
-    //     700: {
+	const slider = tns({
+		container: '.carousel__inner',
+		items: 1,
+		slideBy: 'page',
+		nav: true,
+		navPosition: "bottom",
+		controls: false,
+		autoplay: false,
+	});
 
-    //     },
-    //     900: {
-    //         items: 1,
-    //         slideBy: 'page',
-    //         nav: true,
-    //         controls: true,
-	// 		controlsContainer: false,
-    //     }
-    //   }
-  });
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  });
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
+	// Навигация право, лево (кнопки)
+
+	document.querySelector('.prev').addEventListener('click', function () {
+		slider.goTo('prev');
+	});
+	document.querySelector('.next').addEventListener('click', function () {
+		slider.goTo('next');
+	});
+
+	//Табы на jqury
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+			.addClass('catalog__tab_active')
+			.siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content')
+			.removeClass('catalog__content_active')
+			.eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	// пролистывание блоков на jquery
+
+	// $('.catalog-item__link').each(function(i) {
+	// 		$(this).on('click', function(e) {
+	// 			e.preventDefault();
+	// 			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+	// 			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+	// 	});
+	// });
+	// $('.catalog-item_back').each(function(i) {
+	// 	$(this).on('click', function(e) {
+	// 		e.preventDefault();
+	// 		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+	// 		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+	// 	});
+	// });
 });
+})(jQuery);
+
+	// пролистывание блоков на js
+
+function toggleSlide(item) {
+	$(item).each(function(i) {
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+		});
+	});
+}
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item_back');
