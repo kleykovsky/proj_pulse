de6d0963dc9d6dc3f2d69b282067c2e6ab17806f
Alex <kleykovsky@gmail.com>
2022-04-11 16:40:47 +0500

    finish section adapt: tabs & catalog, comment

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@
 
 (function($) {
 	$(function(){
-
 	const slider = tns({
 		container: '.carousel__inner',
 		items: 1,
@@ -12,29 +11,21 @@
 		controls: false,
 		autoplay: false,
 	});
-
-	// Навигация право, лево (кнопки)
-
+	// Навигация slider блока право, лево (кнопки)
 	document.querySelector('.prev').addEventListener('click', function () {
 		slider.goTo('prev');
 	});
 	document.querySelector('.next').addEventListener('click', function () {
 		slider.goTo('next');
 	});
-
 	//Табы на jqury
-
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 		$(this)
-			.addClass('catalog__tab_active')
-			.siblings().removeClass('catalog__tab_active')
-			.closest('div.container').find('div.catalog__content')
-			.removeClass('catalog__content_active')
-			.eq($(this).index()).addClass('catalog__content_active');
-	});
-
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index())
+			.addClass('catalog__content_active');
+	  });
 	// пролистывание блоков на jquery
-
 	// $('.catalog-item__link').each(function(i) {
 	// 		$(this).on('click', function(e) {
 	// 			e.preventDefault();
@@ -51,17 +42,15 @@
 	// });
 });
 })(jQuery);
-
 	// пролистывание блоков на js
-
 function toggleSlide(item) {
-	$(item).each(function(i) {
-		$(this).on('click', function(e) {
-			e.preventDefault();
-			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			});
 		});
-	});
-}
+	}
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item_back');
